b2db8cfd4ebe8c243829392747efc9e15c2c3b17
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2024-04-13 16:02:04 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -16,34 +16,40 @@ supabase = create_client(DATABASE_URL, DATABASE_KEY)
 app = FastAPI()
 
 # Rota para registrar um usuário
-@app.post("/registrar_usuario")
-async def register_user(username: str = Form(...), password: str = Form(...)):
+@app.post("/inserir_usuario")
+async def inserir_usuario(some_column: str = Form(...), other_column: str = Form(...)):
     try:
-        # Inserir usuário no banco de dados Supabase
-        data = await supabase.table("usuarios").insert({"username": username, "password": password})
-        if data["error"] is not None:
-            raise HTTPException(status_code=500, detail="Erro ao registrar usuário. Por favor, tente novamente.")
+        # Inserir novo usuário na tabela 'usuarios' do banco de dados Supabase
+        response = await supabase.from_("usuarios").insert([
+            {"some_column": some_column, "other_column": other_column}
+        ]).execute()
+
+        # Verificar se houve algum erro na inserção
+        if response.error:
+            raise HTTPException(status_code=500, detail="Erro ao inserir usuário no banco de dados.")
+
+        # Selecionar e retornar os dados inseridos
+        inserted_data = await supabase.from_("usuarios").select("*").execute()
+        return JSONResponse(content=inserted_data.data)
+
     except Exception as e:
-        # Use print para mostrar o erro no console, ou configure o logger
-        print(f"Erro ao registrar usuário: {str(e)}")
-        raise HTTPException(status_code=500, detail="Erro ao registrar usuário. Por favor, tente novamente.")
-    return RedirectResponse("/usuarios.html")
+        print(f"Erro ao inserir usuário no banco de dados: {str(e)}")
+        raise HTTPException(status_code=500, detail="Erro ao inserir usuário no banco de dados.")
 
 # Rota para listar os usuários cadastrados
 @app.get("/usuarios")
 async def listar_usuarios():
     try:
         # Consultar os usuários no banco de dados Supabase
-        response = await supabase.table("usuarios").select("*").execute()
-        if response["error"]:
+        response = await supabase.from_("usuarios").select("*").execute()
+        if response.error:
             raise HTTPException(status_code=500, detail="Erro ao obter usuários do banco de dados.")
 
         # Retornar os usuários como uma resposta JSON
-        return JSONResponse(content=response["data"])
+        return JSONResponse(content=response.data)
     except Exception as e:
         print(f"Erro ao obter usuários do banco de dados: {str(e)}")
         raise HTTPException(status_code=500, detail="Erro ao obter usuários do banco de dados.")
-
 # Rota para excluir um usuário
 @app.delete("/apagar_usuario/{user_id}")
 async def delete_usuario(user_id: int):
