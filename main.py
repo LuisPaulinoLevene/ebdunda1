594685f63f5a2aad4b9dd005cd59a74a80dd61e4
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2024-04-13 14:44:22 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Form, HTTPException
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi.responses import FileResponse, JSONResponse, RedirectResponse
 from dotenv import load_dotenv
 import os
 from supabase import create_client
@@ -12,7 +12,6 @@ DATABASE_KEY = os.getenv("DATABASE_KEY")
 
 # Criar o cliente Supabase
 supabase = create_client(DATABASE_URL, DATABASE_KEY)
-import logging
 
 app = FastAPI()
 
@@ -25,9 +24,10 @@ async def register_user(username: str = Form(...), password: str = Form(...)):
         if data["error"] is not None:
             raise HTTPException(status_code=500, detail="Erro ao registrar usuário. Por favor, tente novamente.")
     except Exception as e:
-        logger.error(f"Erro ao registrar usuário: {str(e)}")
+        # Use print para mostrar o erro no console, ou configure o logger
+        print(f"Erro ao registrar usuário: {str(e)}")
         raise HTTPException(status_code=500, detail="Erro ao registrar usuário. Por favor, tente novamente.")
-    return RedirectResponse("/utilizadores.html")
+    return RedirectResponse("/usuarios.html")
 
 # Rota para listar os usuários cadastrados
 @app.get("/usuarios")
@@ -39,8 +39,9 @@ async def listar_usuarios():
             raise HTTPException(status_code=500, detail="Erro ao obter usuários do banco de dados.")
 
         # Retornar os usuários como uma resposta JSON
-        return response["data"]
+        return JSONResponse(content=response["data"])
     except Exception as e:
+        print(f"Erro ao obter usuários do banco de dados: {str(e)}")
         raise HTTPException(status_code=500, detail="Erro ao obter usuários do banco de dados.")
 
 # Rota para excluir um usuário
@@ -52,6 +53,7 @@ async def delete_usuario(user_id: int):
         if data["error"]:
             raise HTTPException(status_code=500, detail="Erro ao excluir o usuário. Por favor, tente novamente.")
     except Exception as e:
+        print(f"Erro ao excluir o usuário: {str(e)}")
         raise HTTPException(status_code=500, detail="Erro ao excluir o usuário. Por favor, tente novamente.")
     return RedirectResponse("/usuarios.html")
 
@@ -59,7 +61,3 @@ async def delete_usuario(user_id: int):
 @app.get("/", response_class=FileResponse)
 async def read_home():
     return FileResponse("index.html")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
