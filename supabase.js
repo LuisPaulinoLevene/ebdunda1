1691299a7bc156154e64b5a443ebbb64a43bc848
LuisPaulinoLevene <136761438+LuisPaulinoLevene@users.noreply.github.com>
2024-04-13 15:13:20 -0600

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -2,6 +2,6 @@ importar { createClient } de '@supabase/supabase-js'
 
 const supabaseUrl = processo . env .SUPABASE_URL
 const supabaseKey = processo . env . DATABASE_KEY
-const supabase = createClient ( Url , Key )
+const supabase = createClient ( supabaseUrl , supabaseKey )
 
 export default supabase
